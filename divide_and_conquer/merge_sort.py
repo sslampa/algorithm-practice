5cdcd4748bf21bab6a90079e6382f3e6675e39a3
sslampa <sslampa@gmail.com>
2017-04-16 18:51:18 -0700

    Finish inversions problems with Python

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -3,10 +3,12 @@ import math
 
 def merge(a, b):
     combine_list = []
+    start = 0
     while len(a) > 0 and len(b) > 0:
         if a[0] > b[0]:
             combine_list.append(b[0])
             b.pop(0)
+            start += len(a)
         else:
             combine_list.append(a[0])
             a.pop(0)
@@ -19,21 +21,32 @@ def merge(a, b):
         combine_list.append(b[0])
         b.pop(0)
 
-    return combine_list
+    return combine_list, start
 
 
 def merge_sort(a):
     length = len(a)
     if length == 1:
-        return a
+        return a, 0
     l1 = a[0:math.floor(length/2)]
     l2 = a[math.floor(length/2):]
-    l1 = merge_sort(l1)
-    l2 = merge_sort(l2)
+    l1, x = merge_sort(l1)
+    l2, y = merge_sort(l2)
+    result, z = merge(l1, l2)
 
-    return merge(l1, l2)
+    return result, (x + y + z)
 
 
-some_list = [5, 3, 1, 2, 7, 6, 4]
+def answer(f):
+    values = open(f, 'r')
+    test = []
+    for line in values:
+        tmp = line.split('\n')
+        test.append(int(tmp[0]))
+    values.close()
 
-print(merge_sort(some_list))
+    sorted_list, inversions = merge_sort(test)
+    print(inversions)
+
+
+answer('IntegerArray.txt')
